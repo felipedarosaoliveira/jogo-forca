df62d86ff8c44ec91f80a1d1dd2f336e85b864ba
= <=>
2018-10-25 18:30:30 -0200

    types

--- testes.js
+++ testes.js
@@ -2,15 +2,13 @@
   name:string;
 }
 
-function withUID<T extends Person>(obj: T) { 
+function withUID<T extends Person>(obj: T) {
   return Object.assign({}, obj, { uuid: 12 });
 }
 let value = withUID({ name: "POLY", surname: "Chack" }); // is valid */
-
-let t:[string,number] ;
-t = ['3',1];
+var t;
+t = ['3', 1];
 t.push('1');
 t.push(2);
-t.push('3')
+t.push('3');
 console.log(t);
-
